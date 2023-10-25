1e3def22997c8c650b1c44e22237a7a43b39ae94
AmazedWonder <giftedwonda@gmail.com>
2023-10-25 20:40:17 +0100

    update  basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -36,10 +36,7 @@ class BaseModel:
                 elif key != "__class__":
                     setattr(self, key, value)
 
-    def __str__(self):
-        """Returns a string representation of the instance"""
-        cls = (str(type(self)).split('.')[-1]).split('\'')[0]
-        return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
+    
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
@@ -62,3 +59,8 @@ class BaseModel:
     def delete(self):
         """Docs"""
         models.storage.delete(self)
+
+    def __str__(self):
+        """Returns a string representation of the instance"""
+        cls = (str(type(self)).split('.')[-1]).split('\'')[0]
+        return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
