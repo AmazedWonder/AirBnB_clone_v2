0d115745d1fa6064ea547ed66450aaa5e63f3ca2
AmazedWonder <giftedwonda@gmail.com>
2023-11-27 11:02:55 +0100

    Compress before sending

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -3,30 +3,28 @@
    function called do_pack which is used to
    generate a .tgz archive of static files.
 """
-import os
+
 from datetime import datetime
-from fabric.api import local, runs_once
+from fabric.api import *
+from os.path import isdir
+import os
 
 
-@runs_once
 def do_pack():
-    """Archives the static files.Function is only executed once"""
-    if not os.path.isdir("versions"):
-        os.mkdir("versions")
-    d_time = datetime.now()
-    output = "versions/web_static_{}{}{}{}{}{}.tgz".format(
-        d_time.year,
-        d_time.month,
-        d_time.day,
-        d_time.hour,
-        d_time.minute,
-        d_time.second
-    )
+    """generates a tgz archive"""
+    date = datetime.now().strftime("%Y%m%d%H%M%S")
+    if isdir("versions") is False:
+        local("sudo mkdir -p versions")
+    file_name = "versions/web_static_{}.tgz".format(date)
     try:
-        print("Packing web_static to {}".format(output))
-        local("tar -cvzf {} web_static".format(output))
-        size = os.stat(output).st_size
-        print("web_static packed: {} -> {} Bytes".format(output, size))
-    except Exception:
-        output = None
-    return output
+        #date = datetime.now().strftime("%Y%m%d%H%M%S")
+        #if isdir("versions") is False:
+            #local("sudo mkdir -p versions")
+        #file_name = "versions/web_static_{}.tgz".format(date)
+        print("Packing web_static to {}".format(file_name))
+        local("sudo tar -cvzf {} web_static".format(file_name))
+        size = os.stat(file_name).st_size
+        print("web_static packed: {} -> {} Bytes".format(file_name, size))
+        return file_name
+    except:
+        return None
